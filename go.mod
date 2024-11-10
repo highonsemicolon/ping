df46a865d31ddd3f141a17e5a8cb007c49d722cf
onkar <onkar.chendage@gmail.com>
2024-11-10 23:17:20 +0530

    connect to mysql db

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/highonsemicolon/ping
 go 1.23.2
 
 require (
-	github.com/gorilla/mux v1.8.1
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/segmentio/kafka-go v0.4.47
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 )
--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 
+	"github.com/highonsemicolon/ping/user-service/pkg/db"
 	"github.com/highonsemicolon/ping/user-service/pkg/utils"
 )
 
 func main() {
-	_, err := utils.LoadConfig("./configs/config.yaml")
+	config, err := utils.LoadConfig("./configs/config.yaml")
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
+
+	dbConn, err := db.InitMySQL(config.MySQL.DSN)
+	if err != nil {
+		log.Fatalf("Error connecting to MySQL %v", err)
+	}
+	defer dbConn.Close()
 }
